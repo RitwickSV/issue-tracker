20cabdaa3b0706a44174899ec822a25cf5d9a26a
RitwickSV <ritwicksv.ofc@gmail.com>
2024-05-20 17:23:04 +0530

    Axios+Reactform+Controller

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,25 +2,49 @@
 import React from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { Controller, useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssue = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <div className="flex-col space-y-2">
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+      className="flex-col space-y-2"
+    >
       <div>
         <input
           type="text"
           placeholder="Title"
           className="input input-bordered w-full max-w-lg rounded-xl"
+          {...register("title")}
         />
       </div>
       <div>
-        <SimpleMDE
-          className="w-full max-w-lg rounded-2xl"
-          placeholder="Description"
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              className="w-full max-w-lg rounded-2xl"
+              placeholder="Description"
+              {...field}
+            />
+          )}
         />
       </div>
       <button className="btn btn-primary rounded-lg">Add new Issue</button>
-    </div>
+    </form>
   );
 };
 
