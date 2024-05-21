6b3896484ce10a523b98b840fd2db04ca04224b2
RitwickSV <ritwicksv.ofc@gmail.com>
2024-05-21 10:23:39 +0530

    Badges for issue status

---
+++ app/components/IssueStatusBadge.tsx
@@ -0,0 +1,22 @@
+import { Status } from "@prisma/client";
+import React from "react";
+
+interface Props {
+  status: Status;
+}
+
+const badgeColors: Record<
+  Status,
+  { label: string; color: "badge-accent" | "badge-primary" | "badge-secondary" }
+> = {
+  OPEN: { label: "Open", color: "badge-accent" },
+  IN_PROGRESS: { label: "In Progress", color: "badge-secondary" },
+  CLOSED: { label: "Closed", color: "badge-primary" },
+};
+
+const IssueStatusBadge = ({ status }: Props) => {
+  const className = "badge badge-outline " + badgeColors[status].color;
+  return <div className={className}>{badgeColors[status].label}</div>;
+};
+
+export default IssueStatusBadge;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,50 @@
+import prisma from "@/prisma/client";
 import Link from "next/link";
 import React from "react";
+import IssueStatusBadge from "../components/IssueStatusBadge";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
   return (
-    <div>
-      <button className="btn btn-primary">
-        <Link href="/issues/new">Add Issue</Link>
-      </button>
+    <div className="flex-col space-y-5">
+      <div>
+        <button className="btn btn-primary">
+          <Link href="/issues/new">Add Issue</Link>
+        </button>
+      </div>
+      <div className="overflow-x-auto rounded-2xl">
+        <table className="table table-auto ">
+          <thead className="text-lg bg-gray-200">
+            <tr>
+              <td>Issue</td>
+              <td className="hidden md:table-cell">Status</td>
+              <td className="hidden md:table-cell">Created On</td>
+            </tr>
+          </thead>
+          <tbody className="bg-gray-50">
+            {issues.map((issue) => (
+              <tr key={issue.id}>
+                <td>
+                  {issue.title}
+
+                  {
+                    <span className="text-sm md:hidden">
+                      <br />
+                      <IssueStatusBadge status={issue.status} />
+                    </span>
+                  }
+                </td>
+                <td className="hidden md:table-cell">
+                  <IssueStatusBadge status={issue.status} />
+                </td>
+                <td className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
